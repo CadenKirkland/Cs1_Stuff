406730f74b139a6411b488f5021057b99ca84160
CadenKirkland <Ckirk267@gmail.com>
2026-08-31 11:15:33 -0500

    Update Java11TForLoop.java

--- Java11TForLoop.java
+++ Java11TForLoop.java
@@ -2,17 +2,21 @@
 //demonstrate using traditional for loop
 
 public class Java11TForLoop {
-    public static void main(String[] args){
-        for (int x=0; x <=10; x++){
+    public static void main(String[] args) {
+        for (int x = 0; x <= 10; x++) {
 
-            System.out.println("x is "+ x);
+            System.out.println("x is " + x);
             if (x == 7) {
                 System.out.println("7 is a lucky number");
             }
-
         }
         // add another For loop here with z as its iterator
-        // initialize it to 5 and have it print its value until it reaches 15
-        // include an IF statement so tht it prints "hooray" when it equals 12
+        for (int z = 5; z <= 15; z++) {
+
+            System.out.println("x is " + z);
+            if (z == 12) {
+                System.out.println("hooray");
+            }
+        }
     }
 }
